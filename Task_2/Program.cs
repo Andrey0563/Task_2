a1442d3a8e5462373430990c029a238f324f6d63
Andrey <duzhak.a55@gmail.com>
2020-12-07 17:59:11 +0200

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the number type int: ");
+                int a = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the number type double: ");
+                double b = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the number type long: ");
+                long c = long.Parse(Console.ReadLine());
+
+                Console.WriteLine($"a = {a}; b = {b}; c = {c}");
+            }
+            catch
+            {
+                Console.WriteLine("Exaption");
+            }
+
+        }
+    }
+}
